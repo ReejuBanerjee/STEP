ffd62b90fbcc3ce758a5907facee99a6d9de1e48
Reeju Banerjee <reejubanerjee15@gmail.com>
2026-03-03 09:58:16 +0530

    Updated code logic

--- UC 7/OOPSBannerApp.java
+++ UC 7/OOPSBannerApp.java
@@ -5,7 +5,7 @@
  * character-to-pattern mappings. The application retrieves and displays the "OOPS"
  * banner using these mappings. This approach enhances code organization and modularity.
  *
- * @author Mallikarjun Shankar
+ * @author Reeju
  * @version 7
  */
 
